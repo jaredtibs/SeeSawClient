f31f8ec35284692d0743f8ec5b271cccb3f32396
Jared Tibshraeny <jmtibs@gmail.com>
2017-03-26 20:07:07 -0700

    separate out share button into its own component

--- src/components/Location.js
+++ src/components/Location.js
@@ -13,6 +13,7 @@ import {
   RefreshControl
 } from 'react-native';
 
+import ShareButton from '../components/ShareButton';
 import FeedContainer from '../containers/FeedContainer';
 import Icon from 'react-native-vector-icons/Ionicons';
 import ParallaxView from 'react-native-parallax-view';
@@ -115,24 +116,7 @@ class Location extends Component {
             { !isFetching ? this.renderFeed() : null}
         </ParallaxView>
 
-        <View style={styles.shareButton}>
-          <TouchableHighlight
-            onPress={() => this._openInputForm()}
-            underlayColor='#FFFFFF'
-            style={styles.textInputButton}>
-
-            <View style={styles.inputText}>
-              <Icon name='md-add' size={18} style={styles.plusIcon}></Icon>
-              <Text style={styles.placeholder}>
-                Share a thought...
-              </Text>
-            </View>
-          </TouchableHighlight>
-          <TouchableHighlight
-            style={styles.cameraButton}>
-            <Icon name='ios-camera-outline' size={25} style={styles.cameraIcon}></Icon>
-          </TouchableHighlight>
-        </View>
+        <ShareButton />
       </View>
     )
   }
@@ -191,62 +175,7 @@ const styles = StyleSheet.create({
   statValue: {
     fontSize: 18,
     fontFamily: 'GTPressuraMonoTrial-Bold',
-  },
-
-  shareButton: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: 55,
-    backgroundColor: '#FFFFFF',
-    borderColor: 'rgba(56, 55, 61, 0.2)',
-    shadowOffset: {
-      width: 1,
-      height: 1
-    },
-    shadowColor: 'rgba(0,0,0,0.26)',
-    shadowOpacity: 0.5,
-    padding: 10
-  },
-
-  textInputButton: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-start'
-  },
-
-  inputText: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  placeholder: {
-    fontSize: 14,
-    fontFamily: 'MaisonNeueTRIAL-Medium',
-    color: 'rgba(0,0,0,.30)',
-    paddingLeft: 15
-  },
-
-  plusIcon: {
-    color: '#343442',
-    paddingLeft: 10
-  },
-
-  cameraButton: {
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  cameraIcon: {
-    color: '#343442',
-    paddingRight: 5
   }
-
 });
 
 export default Location;
---
+++ src/components/ShareButton.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+const ImagePicker = require('react-native-image-picker');
+
+class ShareButton extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <View style={styles.shareButton}>
+          <TouchableHighlight
+            onPress={() => this._openInputForm()}
+            underlayColor='#FFFFFF'
+            style={styles.textInputButton}>
+
+            <View style={styles.inputText}>
+              <Icon name='md-add' size={18} style={styles.plusIcon}></Icon>
+              <Text style={styles.placeholder}>
+                Share a thought...
+              </Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={styles.cameraButton}
+            onPress={() => this._openCamera}>
+            <Icon name='ios-camera-outline' size={25} style={styles.cameraIcon}></Icon>
+          </TouchableHighlight>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  shareButton: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 55,
+    backgroundColor: '#FFFFFF',
+    borderColor: 'rgba(56, 55, 61, 0.2)',
+    shadowOffset: {
+      width: 1,
+      height: 1
+    },
+    shadowColor: 'rgba(0,0,0,0.26)',
+    shadowOpacity: 0.5,
+    padding: 10
+  },
+
+  textInputButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start'
+  },
+
+  inputText: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  placeholder: {
+    fontSize: 14,
+    fontFamily: 'MaisonNeueTRIAL-Medium',
+    color: 'rgba(0,0,0,.30)',
+    paddingLeft: 15
+  },
+
+  plusIcon: {
+    color: '#343442',
+    paddingLeft: 10
+  },
+
+  cameraButton: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  cameraIcon: {
+    color: '#343442',
+    paddingRight: 5
+  }
+
+});
+
+export default ShareButton;
